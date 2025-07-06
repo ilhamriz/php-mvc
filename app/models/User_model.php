26ae775c9685886ac250a27aa6a136e1ceead8b9
ilhamriz <milhamrizky7@gmail.com>
2025-07-06 10:07:12 +0700

    7. Model

---
+++ app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model
+{
+  private $nama = "Ilham";
+
+  public function getUser()
+  {
+    return $this->nama;
+  }
+}
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,4 @@
 <div class="container">
   <h1 class="mt-4">Hello</h1>
+  <p>Saya <?= $data['nama']; ?></p>
 </div>
