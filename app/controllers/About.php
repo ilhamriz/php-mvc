766495fbcde1606184d52d4377f4eda3185e7a2b
ilhamriz <milhamrizky7@gmail.com>
2025-07-06 08:56:17 +0700

    5. View

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,14 +1,23 @@
 <?php
 
-class About
+class About extends Controller
 {
   public function index($nama = "Ilham", $pekerjaan = "Developer")
   {
-    echo "Halo, saya $nama. Saya seorang $pekerjaan";
+    $data['title'] = "About";
+    $data['nama'] = $nama;
+    $data['pekerjaan'] = $pekerjaan;
+
+    $this->view("templates/header", $data);
+    $this->view("about/index", $data);
+    $this->view("templates/footer");
   }
 
   public function page()
   {
-    echo "about/page";
+    $data['title'] = "Pages";
+    $this->view("templates/header", $data);
+    $this->view("about/page");
+    $this->view("templates/footer");
   }
 }
